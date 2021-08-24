a8a4104e488473eb4fee4c861f152beb99b32593
LAKSHMI PRIYA K <79240459+Priyakamesh4899@users.noreply.github.com>
2021-08-24 22:07:26 +0530

    Delete BookStore directory

--- BookStore/src/main/java/com/revature/bookproject/repository/OrderDetailsJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.revature.bookproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.bookproject.model.OrderDetails;
-public interface OrderDetailsJpaRepository extends JpaRepository<OrderDetails,Integer>{
-
-}
